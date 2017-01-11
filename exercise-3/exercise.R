37c09e720096558d40be51622996a4a9a9a89ad7
Kelvin Ka Ho Li <kelvinli@uw.edu>
2017-01-11 14:35:17 -0800

    answer the questions for tuesday lecture 2nd attempt

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -11,30 +11,33 @@ print(name.length)
 
 # Create a variable `now.doing` that is your named followed by "is programming!" to the end of your name
 # (use the `paste()` function)
-now.doing = 
+now.doing = "Kelvin is programming"
 
 # Make the `now.doing` variable upper case
-
+toupper(now.doing)
 
 ### Bonus
 
 # Pick two of your favorite numbers (between 1 and 100) and assign them to `fav.1` and `fav.2`
-
+fav.1 = 55
+fav.2 = 53
 
 # Divide each number by the square root of 201 and save it as the same variable
-
+fav.1 = fav.1/sqrt(201)
+fav.2 = fav.2/sqrt(201)
 
 # Create a variable `raw.sum` that is the sum of those two variables. Use the `sum()` function for practice
-
+raw.sum = fav.1 + fav.2
 
 # Create a variable `round.sum` that is the `raw.sum` rounded to 1 decimal place. Use the `round()` function
-
-
+round.sum = round(raw.sum,digits = 1)
 # Create two new variables `round.1` and `round.2` that are your `fav.1` and `fav.2` variables rounded
 # to 1 decimal place
-
+round.1 = round(fav.1, digits=1)
+round.2 = round(fav.2,digits=1)
 
 # Create a variable `sum.round` that is the sum of the rounded values
-
+sum.round = round.1 + round.2
 
 # Which is bigger, `round.sum` or `sum.round`? (You can use the `max()` function!)
+max(round.sum,sum.round)
